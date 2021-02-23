669e8830ba215f33c37e645e594b9dc6e0d26c56
ZohanBG <76525041+ZohanBG@users.noreply.github.com>
2021-02-23 18:37:01 +0200

    C# Advanced Exam - 22 Feb 2020

---
+++ CSharp Advanced/Advanced Exam - 22 Feb 2020/Lootbox/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lootbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Queue<int> firstBox = new Queue<int>(input);
+            input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            Stack<int> secondBox = new Stack<int>(input);
+            int valueSum = 0;
+            while (firstBox.Count > 0 && secondBox.Count > 0)
+            {
+                int sum = firstBox.Peek() + secondBox.Peek();
+                if (sum % 2 == 0)
+                {
+                    valueSum += sum;
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    firstBox.Enqueue(secondBox.Pop());
+                }
+            }
+            if (firstBox.Count <= 0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+            if (valueSum >= 100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {valueSum}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {valueSum}");
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/Advanced Exam - 22 Feb 2020/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advanced_Exam___22_Feb_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
